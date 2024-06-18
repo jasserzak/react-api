8df60d2b3c11df87e8fa4105218dfec7fffe75fb
Jasserzak <jasserslimani01@gmail.com>
2024-06-18 23:25:35 +0100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,37 @@
+// src/UserList.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setListOfUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchUsers();
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <div className="user-list">
+      <h2>List of Users</h2>
+      <ul>
+        {listOfUsers.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
